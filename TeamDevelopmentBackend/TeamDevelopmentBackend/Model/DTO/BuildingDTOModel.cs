04e3577237e2fa964c4b168a6d19213a475cf0c8
Trushkin Artem <at.120@ya.ru>
2023-02-21 01:09:42 +0700

    building and room dtos updated constructors has been added to convert them from db models

--- TeamDevelopmentBackend/TeamDevelopmentBackend/Model/DTO/BuildingDTOModel.cs
+++ TeamDevelopmentBackend/TeamDevelopmentBackend/Model/DTO/BuildingDTOModel.cs
@@ -2,5 +2,13 @@
 {
     public class BuildingDTOModel
     {
+        public Guid id { get; set; }
+        public string title { get; set; }
+
+        public BuildingDTOModel(BuildingDbModel building)
+        {
+            id = building.Id;
+            title = building.Name;
+        }
     }
 }
