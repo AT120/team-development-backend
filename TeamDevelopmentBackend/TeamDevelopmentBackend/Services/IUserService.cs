e399cc7cdf626c85f573af7db0fd83e7ca987a50
Alexey <lesha.braun1029@gmail.com>
2023-02-26 16:59:11 +0700

    addRolesAdder

---
+++ TeamDevelopmentBackend/TeamDevelopmentBackend/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using TeamDevelopmentBackend.Model;
+using TeamDevelopmentBackend.Model.DTO;
+
+namespace TeamDevelopmentBackend.Services
+{
+    public interface IUserService
+    {
+        public Task GiveUserARole(LoginDTOModel userLogin, Role role);
+    }
+}
---
+++ TeamDevelopmentBackend/TeamDevelopmentBackend/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using TeamDevelopmentBackend.Model;
+using TeamDevelopmentBackend.Model.DTO;
+
+namespace TeamDevelopmentBackend.Services
+{
+    public class UserService : IUserService
+    {
+        private DefaultDBContext _dbContext;
+        public UserService(DefaultDBContext dBContext) 
+        {
+        _dbContext= dBContext;
+        }
+        public async Task GiveUserARole(LoginDTOModel userLogin, Role role)
+        {
+            var user = _dbContext.Users.FirstOrDefault(x => x.Login == userLogin.Login);
+            if(user == null)
+            {
+                throw new ArgumentException("There is no user with this login!");
+            }
+            else
+            {
+                user.Role = role;
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
